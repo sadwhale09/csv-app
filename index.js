a572abef57ef0dbacf6ac592581268e59a14a8f8
sadwhale09 <dev.jraniszewski@pm.me>
2023-08-06 12:16:39 +0200

    Add everything

---
+++ index.js
@@ -0,0 +1,78 @@
+document.body.onload = addRow;
+
+let count = 0;
+function addRow() {
+
+    const table = document.getElementById("table");
+
+    table.insertAdjacentHTML("beforeend", `
+        <tr id="row` + count + `">
+        <td class="content">
+        <textarea class="input" style="resize:none; text-align:center; text-valign:center"></textarea>
+        </td>
+        <td class="content">
+        <textarea class="input" style="resize:none; text-align:center; text-valign:center"></textarea>
+        </td>
+        <td>
+        <button id="rmBtn` + count + `" type="button" title="Remove row." onClick="removeRow()">X</button>
+        </td>
+        </tr>
+    `);
+
+    count += 1;
+}
+
+// For now it remove rows from start
+// TODO: Make the button remove a row it belongs to
+function removeRow() {
+    const buttons = document.getElementsByTagName("button");
+
+    const buttonPressed = e => { 
+        const child = document.getElementById(e.target.id);
+        child?.parentElement.parentElement.parentElement?.remove();
+    }
+
+    for (let button of buttons) {
+        button.addEventListener("click", buttonPressed);
+    }
+}
+
+function parseData() {
+    var csv_data = [];
+
+    var rows = document.getElementsByTagName('tr');
+    for (var i = 1; i < rows.length; i++) {
+
+        var cols = rows[i].querySelectorAll('td textarea');
+
+        var csvrow = [];
+        for (var j = 0; j < cols.length; j++) {
+
+            csvrow.push(cols[j].value);
+        }
+        csv_data.push(csvrow.join("|"));
+    }
+    csv_data = csv_data.join('\n');
+
+    download(csv_data);
+
+}
+
+function download(csv_data) {
+    CSVFile = new Blob([csv_data], {
+        type: "text/csv"
+    });
+
+    var temp_link = document.createElement('a');
+
+    temp_link.download = "test.csv";
+    var url = window.URL.createObjectURL(CSVFile);
+    temp_link.href = url;
+
+    temp_link.style.display = "none";
+    document.body.appendChild(temp_link);
+
+    temp_link.click();
+    document.body.removeChild(temp_link);
+}
+
